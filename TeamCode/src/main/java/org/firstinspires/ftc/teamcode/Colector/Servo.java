0f54334ac021872315559e1e3c2e23939b16da86
Edward77-code <70574532+Edward77-code@users.noreply.github.com>
2022-01-15 14:08:22 +0200

    servo update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Colector/Servo.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Colector/Servo.java
@@ -1,4 +1,31 @@
 package org.firstinspires.ftc.teamcode.Colector;
 
-public class Servo {
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.CRServo;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+
+@TeleOp
+public class Servo extends LinearOpMode {
+
+    @Override
+    public void runOpMode()
+    {
+        CRServo servo = hardwareMap.crservo.get("back_servo");
+        servo.setDirection(DcMotorSimple.Direction.FORWARD);
+
+        waitForStart();
+
+        while(opModeIsActive())
+        {
+            servo.setPower(1.0);
+
+            telemetry.update();
+        }
+    }
+
 }
+
