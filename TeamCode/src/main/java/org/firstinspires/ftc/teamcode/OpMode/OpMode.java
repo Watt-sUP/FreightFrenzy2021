2a2de4a00a1bfbe5344e2d90e0fda39517d30df9
Edward77-code <70574532+Edward77-code@users.noreply.github.com>
2022-01-25 15:20:11 +0200

    OpMode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpMode/OpMode.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode.OpMode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+@TeleOp(name = "OpMode", group = "OpMode")
+public class OpMode extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        while(opModeIsActive()) {
+            
+        }
+
+    }
+}
