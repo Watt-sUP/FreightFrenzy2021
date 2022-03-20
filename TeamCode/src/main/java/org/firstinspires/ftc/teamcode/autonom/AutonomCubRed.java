4cd34cd09770ca31c3a7166a3a1203663aeeddcb
Darius Jipa <jipad16@yahoo.com>
2022-03-20 14:45:55 +0200

    Actualizat traiectorii roadrunner

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonom/AutonomCubRed.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/autonom/AutonomCubRed.java
@@ -22,7 +22,7 @@ public class AutonomCubRed extends LinearOpMode {
         SampleMecanumDrive drive = new SampleMecanumDrive(hardwareMap);
         Mugurel robot = new Mugurel(hardwareMap);
 
-        Pose2d startPose = new Pose2d(12, -62, Math.toRadians(90));
+        Pose2d startPose = new Pose2d(12, -63, Math.toRadians(90));
 
         drive.setPoseEstimate(startPose);
 
@@ -31,25 +31,22 @@ public class AutonomCubRed extends LinearOpMode {
         */
 
         Trajectory delivery = drive.trajectoryBuilder(startPose)
-                .lineToLinearHeading(new Pose2d(8, -45, Math.toRadians(125)))
+                .splineToLinearHeading(new Pose2d(-12, -48, Math.toRadians(12)), Math.toRadians(45))
                 .build();
-
 
 
         Trajectory supply = drive.trajectoryBuilder(delivery.end())
                 .addTemporalMarker(0.5, () -> robot.cupa.servo.setPosition(0.97))
                 .addTemporalMarker(0.7, () -> robot.glisiere.setToPosition(0))
-                .lineToLinearHeading(new Pose2d(12, -66, Math.toRadians(180)))
+                .splineToSplineHeading(new Pose2d(25, -65, Math.toRadians(180)), Math.toRadians(0))
+                .splineTo(new Vector2d(55, -65.5), Math.toRadians(0))
                 .build();
 
-        Trajectory move = drive.trajectoryBuilder(supply.end())
-                .back(35)
+        Trajectory returnToDelivery = drive.trajectoryBuilder(supply.end(), true)
+                .splineToSplineHeading(new Pose2d(25, -65, Math.toRadians(180)), Math.toRadians(0))
+                .splineToLinearHeading(new Pose2d(-12, -48, Math.toRadians(12)), Math.toRadians(45))
                 .build();
 
-        Trajectory returnToOrigin = drive.trajectoryBuilder(move.end())
-                .addTemporalMarker(0.5, () -> robot.maturica.toggleEject())
-                .forward(35)
-                .build();
 
         waitForStart();
 
@@ -62,40 +59,36 @@ public class AutonomCubRed extends LinearOpMode {
         sleep(300);
         robot.maturica.toggleCollect();
         drive.followTrajectory(supply);
-        drive.followTrajectory(move);
         robot.cupa.toggleDeget();
         robot.glisiere.setToPosition(3);
         sleep(500);
         robot.cupa.servo.setPosition(0.5);
-        drive.followTrajectory(returnToOrigin);
+        drive.followTrajectory(returnToDelivery);
         drive.followTrajectory(delivery);
         robot.cupa.toggleDeget();
         sleep(300);
         robot.maturica.toggleCollect();
         drive.followTrajectory(supply);
-        drive.followTrajectory(move);
         robot.cupa.toggleDeget();
         robot.glisiere.setToPosition(3);
         sleep(500);
         robot.cupa.servo.setPosition(0.5);
-        drive.followTrajectory(returnToOrigin);
+        drive.followTrajectory(returnToDelivery);
         drive.followTrajectory(delivery);
         robot.cupa.toggleDeget();
         sleep(300);
         robot.maturica.toggleCollect();
         drive.followTrajectory(supply);
-        drive.followTrajectory(move);
         robot.cupa.toggleDeget();
         robot.glisiere.setToPosition(3);
         sleep(500);
         robot.cupa.servo.setPosition(0.5);
-        drive.followTrajectory(returnToOrigin);
+        drive.followTrajectory(returnToDelivery);
         drive.followTrajectory(delivery);
         robot.cupa.toggleDeget();
         sleep(300);
         robot.maturica.toggleCollect();
         drive.followTrajectory(supply);
-        drive.followTrajectory(move);
         /**/
         /*
         Cub mijloc
